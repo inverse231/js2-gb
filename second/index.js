673c0b51bdee468d6835f5957c862e9129bba171
inverse231 <3xinverse@gmail.com>
2019-12-12 11:27:14 +0300

    added chat interaction

--- second/index.js
+++ second/index.js
@@ -24,9 +24,9 @@ class Cart {
     constructor() {
         this.items = [];
     }
-    addCartItem(item) {}
-    deleteCartItem(item) {}
-    changeItemQuantity(item) {}
+    addCartItem(item) { }
+    deleteCartItem(item) { }
+    changeItemQuantity(item) { }
 }
 
 class GoodsList {
@@ -35,21 +35,21 @@ class GoodsList {
     }
     fetchGoods() {
         this.goods = [{
-                title: 'Shirt',
-                price: 150
-            },
-            {
-                title: 'Socks',
-                price: 150
-            },
-            {
-                title: 'Jacket',
-                price: 150
-            },
-            {
-                title: 'Shoes',
-                price: 150
-            },
+            title: 'Shirt',
+            price: 150
+        },
+        {
+            title: 'Socks',
+            price: 150
+        },
+        {
+            title: 'Jacket',
+            price: 150
+        },
+        {
+            title: 'Shoes',
+            price: 150
+        },
         ];
     }
     render() {
@@ -68,6 +68,66 @@ class GoodsList {
         return sum;
     }
 }
+
+//Chat stuff
+const messageBox = document.querySelector('.message-box');
+class ChatMessage {
+    constructor(text) {
+        let today = new Date();
+        this.text = text;
+        if (today.getMinutes() < 10) {
+            this.time = today.getHours() + ':0' + today.getMinutes();
+        }
+        else {
+            this.time = today.getHours() + ':' + today.getMinutes();
+        }
+    }
+    renderMessage() {
+        return `<div class="new-sent-message"><span class="sent-message">${this.text}</span><span class="sent-time">${this.time}</span></div>`
+    }
+}
+
+class Chat {
+    constructor(element) {
+        this.element = element;
+        this.messages = [];
+        this.messageBox = messageBox;
+    }
+    sendMessage(message) {
+        message = new ChatMessage(message);
+        this.element.innerHTML += message.renderMessage();
+    }
+}
+
+const mainChatWindow = document.querySelector('.main-chat-window');
+const chat = new Chat(mainChatWindow);
+messageBox.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        chat.sendMessage(this.value);
+        this.value = "";
+    }
+});
+
+
+let cross = document.querySelector('.chat-cross');
+let band = document.querySelector('.chat-band');
+let chatWindow = document.querySelector('.chat-window');
+
+function openChat() {
+    chatWindow.style.display = 'flex';
+    cross.style.alignSelf = 'flex-start';
+    cross.style.display = 'block';
+    band.style.display = 'none';
+}
+function closeChat() {
+    chatWindow.style.display = 'none';
+    cross.style.alignSelf = 'flex-end';
+    cross.style.display = 'none';
+    band.style.display = 'flex';
+}
+band.addEventListener("click", openChat);
+cross.addEventListener("click", closeChat);
 
 const list = new GoodsList();
 list.fetchGoods();
